83608eb8a62d8172b9c7ea2acfbfa48fcd85f8f9
Matheus de Souza <matheusgds3011@gmail.com>
2026-03-09 20:30:58 -0300

    Feat: Adicionando interação com botão upload Imagem

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let inputBtn = document.querySelector('.botao-carregar-imagem');
+let inputFile = document.querySelector('#input-imagem');
+
+inputBtn.addEventListener('click', function() {
+    inputFile.click();
+});
