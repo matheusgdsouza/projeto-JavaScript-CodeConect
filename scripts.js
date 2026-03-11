26b4e031d73e2f7370954dbc53ca8728b614ec86
Matheus de Souza <matheusgds3011@gmail.com>
2026-03-11 16:27:17 -0300

    Feat: Criando Promise responsavel pela leitura da img

--- scripts.js
+++ scripts.js
@@ -4,3 +4,19 @@ const uploadInput = document.querySelector('#input-imagem');
 uploadBtn.addEventListener('click', function() {
     uploadInput.click();
 });
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+
+        leitor.onload = () => {
+            resolve({url: leitor.result, nome: arquivo.name});
+        };
+
+        leitor.onerror = () => {
+            reject(`Houve um erro ao ler o arquivo: ${arquivo.name}`);
+        };
+
+        leitor.readAsDataURL(arquivo);
+    })
+}
